7238b82c217d89529e82f0b8ef0b8a7038dd1ef4
Dana Sevcik <danasevcik@hotmail.com>
2019-12-11 11:28:46 -0500

    found user via email

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
-
+// Login with LinkedIn - the screen should show the icon, when clicked it should show a small pop up requesting permission from current user to share his\her basic profile details, once approved, it should display profile lite details. Info on Profile lite can be found here - https://docs.microsoft.com/en-us/linkedin/shared/references/v2/profile/lite-profile
+// Find linked in page using email - another alternative is to find LinkedIn profile page using email (not via the API), can you think of a way to do so and show that in the app?
 class App extends Component {
 
   state = {
-    code: '',
+    email: '',
     firstName: null,
     lastName: null,
     id: null,
     pic: null
+  }
+
+  componentDidMount() {
+    let authcode = window.location.href.slice(28, -10);
+      if (authcode) {
+      fetch(`https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&code=${authcode}&redirect_uri=http://localhost:3000/&client_id=77i0orwyc6pvp2&client_secret=WvZdgMEKMohjJUqK`)
+        .then(res => res.json())
+        .then(res => this.getProfile(res))
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({email: e.target.value})
   }
 
 
@@ -22,9 +36,18 @@ class App extends Component {
     this.setState({code: ''})
   }
 
-  getProfile = (res) => {
-    console.log(res);
-    fetch(`https://api.linkedin.com/v2/me?oauth2_access_token=${res.access_token}`)
+  handleEmail = () => {
+    console.log(this.state.email);
+    // let token = window.location.href.slice(28, -10);
+    // fetch(`https://www.linkedin.com/sales/gmail/profile/proxy/${this.state.email}`)
+    // .then(res => res.json())
+    // .then(res => console.log(res))
+    // fetch(`https://api.linkedin.com/v2/people/email=${this.state.email}?X-RestLi-Protocol-Version:2.0.0&oauth2_access_token=${this.state.accessToken}`)
+  }
+
+  getProfile = (token) => {
+    console.log(token);
+    fetch(`https://api.linkedin.com/v2/me?oauth2_access_token=${token.access_token}`)
     .then(res => res.json())
     .then(res => this.setState({
       firstName: res.localizedFirstName,
@@ -32,29 +55,26 @@ class App extends Component {
       id: res.id,
       pic: res.profilePicture.displayImage
     }))
-
+    this.setState({accessToken: token.access_token})
   }
-
-  handleChange = (e) => {
-    this.setState({code: e.target.value})
-  }
-
 
   render() {
-    console.log(this.state);
+    console.log('state',this.state);
     return (
       <div className="App">
-        <a target='blank' href='https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=77i0orwyc6pvp2&redirect_uri=https://www.hunterz.io/about&state=xyz&scope=r_liteprofile'>click me for linkedin</a>
-        <form id='get-info' onSubmit={this.handleSubmit}>
-          <label>Once you've logged in with likedin, input here the characters after "code="</label>
-          <input type='text' value={this.state.code} onChange={this.handleChange}/>
-          <button>Let's Go!</button>
-        </form>
+        <a target='blank' href='https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=77i0orwyc6pvp2&redirect_uri=http://localhost:3000/&state=xyz&scope=r_liteprofile,r_emailaddress'>click me for linkedin</a>
+        {this.state.accessToken &&
+          <div>
+            <input value={this.state.email} onChange={this.handleChange}></input>
+            <button onClick={this.handleEmail}>submit</button>
+            <a target="_blank" href={`https://www.linkedin.com/sales/gmail/profile/proxy/${this.state.email}`}>go!</a>
+          </div>
+        }
         {this.state.firstName ? <h1>You are now sharing the following info with Dana-test-app</h1> : <h1>Currently not sharing data with Dana-test-app</h1>}
         {this.state.firstName && <p>First Name: {this.state.firstName}</p>}
         {this.state.lastName && <p>Last Name: {this.state.lastName}</p>}
         {this.state.id && <p>LinkedIn ID: {this.state.id}</p>}
-        {this.state.pic && <p>Meta-Data re: LinkedIn Photo: {this.state.pic}</p>}
+        {this.state.pic && <p>Photo Info: {this.state.pic}</p>}
       </div>
     );
   }
